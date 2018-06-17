45167bd8b58c9207580e9e425ff434ace80048e5
Sam <thesamreaves@gmail.com>
2018-06-17 16:14:59 -0400

    Added word list section of hangman game

---
+++ lib/dictionary.ex
@@ -0,0 +1,34 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for Dictionary.
+  """
+
+
+  @doc """
+  Random Word
+
+  Generates a random word by grabbing a random word from word list
+
+  """
+  def random_word do
+      word_list()
+      |> Enum.random
+  end
+
+
+
+  @doc """
+  Word List
+
+  Generates a list of words by reading from word list at assets/words.txt.
+  Throws an exception if failure.
+
+  """
+  def word_list do
+
+      # Read from word list, and split into
+      "assets/words.txt"
+      |> File.read!
+      |> String.split(~r/\n/)
+  end
+end
